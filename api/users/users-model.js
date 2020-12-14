017ba904ab70e7f483db34f02fda8f4194391141
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-14 17:00:23 -0600

    Starting endpoing testing with Postman!

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -2,18 +2,22 @@ const db = require('../../database/connection');
 
 module.exports = {
     add,
-    find() {
-        return db('userCreds').select('id', 'username').orderBy('id');
-    },
-    findBy(filter) {
-        return db('userCreds').where(filter).orderBy('id');
-    },
+    find, 
+    findBy,
     findById,
 };
 
 async function add(user) {
     const [id] = await db('userCreds').insert(user, 'id');
     return findById(id);
+}
+
+function find() {
+    return db('userCreds').select('id', 'username');
+}
+
+function findBy(filter) {
+    return db('userCreds').where(filter).orderBy('id');
 }
 
 function findById(id) {
