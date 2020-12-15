f71aa4db2849bdfb67cecaa2a6d8b6bb1969efaf
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-14 19:08:34 -0600

    Refactored Users GET, tested functional in Postman!

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,12 +5,20 @@ const User = require('./users-model');
 /// USERS ENDPOINT(S(?)) ///
 // Get a list of all users. 
 router.get('/', checkUserLoggedIn, (req, res) => {
-    try {
-        const fetchUsers = User.find();
-        res.status(200).json(fetchUsers);
-    } catch (err) {
-        res.json(500).json({ message: err.message });
-    }
+    User.find()
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+
+    // try {
+    //     const fetchUsers = User.find();
+    //     res.status(200).json(fetchUsers);
+    // } catch (err) {
+    //     res.json(500).json({ message: err.message });
+    // }
 });
 
 module.exports = router;
